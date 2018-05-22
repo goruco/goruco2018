a67419518a90e85105cef2762e00c40aba86ef41
Jesse Chan-Norris <jcn@pith.org>
2018-05-21 20:52:16 -0400

    Pop up the speaker modal when linking to the homepage with #speaker-name

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -7,4 +7,18 @@ $(document).ready(function(){
     variableWidth: true,
     autoplay: true
   });
+
+  if (window.location.hash) {
+    if ($(window.location.hash+'.modal').size() > 0) {
+      $(window.location.hash).modal();
+    }
+  }
+
+  $('.modal').on('hidden.bs.modal', function(e) {
+    if (history.pushState) {
+      history.pushState("", document.title, window.location.pathname + window.location.search);
+    } else {
+      location.hash = '';
+    }
+  })
 });
